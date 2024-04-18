d116b900cf3bc6f37ad83f1edadff570688b6920
ignajo <ignacio.villagrah@gmail.com>
2024-04-18 11:22:36 -0400

    'Create new functional components and implement behavior'

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order your favourie food here</h2>
+            <p>DeliFácil es un servicio de delivery de comida rápido y conveniente que ofrece una amplia 
+            variedad de opciones gastronómicas para satisfacer todos los antojos. Desde comida saludable 
+            y gourmet hasta platos reconfortantes y deliciosos postres, en DeliFácil tenemos algo para 
+            todos los gustos.
+            </p>
+            <button>View Menu</button>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Header
